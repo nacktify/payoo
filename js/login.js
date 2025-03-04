ac235653b4e43b4ace2b79790e332f282b88367e
Nayemur <nayemianofficial@gmail.com>
2025-03-05 03:29:32 +0600

    added validation by event handler

--- js/login.js
+++ js/login.js
@@ -3,9 +3,17 @@ document.getElementById('btn-login').addEventListener('click', function (event) 
 
     //step 2: stop reloading(stop default behavior of form).
     event.preventDefault();
-    console.log('is ok?')
 
-    //step 3: get the phone number.
+    //step 3: get the phone number and pin number.
     const mobileNumber = document.getElementById('phone-numb').value;
-    console.log(mobileNumber)
+    const pinNumber = document.getElementById('pin-numb').value;
+    console.log(mobileNumber, pinNumber)
+
+    //step 4: validate phone and pin
+    if (mobileNumber === '2580' && pinNumber === '2580') {
+        //step 5: allow user to use the website
+        console.log('You are logged in!')
+    } else {
+        alert('Wrong credentials! Try again.')
+    }
 })
